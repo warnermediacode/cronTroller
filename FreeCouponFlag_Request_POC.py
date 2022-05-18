21631eec2b5dca5d067667e0e5b173fbbc1e6473
tecnoboyer <ingenieroboyer@gmail.com>
2022-05-18 15:35:24 -0400

    adding freeCouponflag

---
+++ FreeCouponFlag_Request_POC.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##### Vindicia teams request
+# 
+# This script finds users who have a free coupon that actually gives access to the content and assigns the category description of this coupon by adding the "coupon_category" field along with the day of the assignment.
+
+import os
+from pymongo import MongoClient
+import pymongo
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import numpy as np
+import csv
+from bson.objectid import ObjectId
+import bson.objectid
+import requests
+import json 
+from pandas.io.json import json_normalize
+
+
+#getting some env var
+string_product=os.environ.get('string_product','localhost:27017')
+db_product=os.environ.get('prod_env_db','local')
+read_preferences='secondaryPreferred'
+API_Login=os.environ.get('prod_env_API_Login','local')
+userAuth=os.environ.get('prod_env_user','local')
+pass_userAuth=os.environ.get('prod_env_past_user','local')
+
+# ### Obteniendo las categorias de cupones del Redis!!!
+
+MensToken = requests.post(API_Login,
+  headers={
+    'Content-Type': 'application/json'
+  },
+  json={
+      "email": userAuth,
+      "password": pass_userAuth,
+      "appId": "ecdf-mobile"
+  }
+)
+
+
+MensToken_token=json.loads(MensToken.text)
+MensToken_token2=pd.json_normalize(MensToken_token)
+MensToken_token2['token'] = MensToken_token2['token'].map(str)
+token=MensToken_token2.token.astype(str)
+token=MensToken_token2.values[0]
+token2=token[1]
+token_beared = token2[:0] + 'Bearer '+token2[:]
+
+
+response2 = requests.get(,
+  headers={
+    'Content-Type': 'application/json',
+    'Authorization': token_beared
+  }
+)
+data = json.loads(response2.text) # carga el string como json
+categorias = pd.json_normalize(data=data['message']) # Desenvaina el json embebido
+
+
+categorias = pd.json_normalize(data=data['message'])
+
+categorias
--- monolithic_v3.py
+++ monolithic_v3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import os
 from pymongo import MongoClient
 import pymongo
@@ -16,15 +13,9 @@ from bson.objectid import ObjectId
 import bson.objectid
 
 
-# In[ ]:
-
-
 string_product=os.environ.get('string_product','localhost:27017')
 db_product=os.environ.get('prod_env_db','local')
 read_preferences='secondaryPreferred'
-
-
-# In[ ]:
 
 
 myclient_con= pymongo.MongoClient(string_product,readPreference=read_preferences)
@@ -34,14 +25,8 @@ InitDay=datetime(2017,1,1,0,0,0)
 hoy=datetime.now()
 
 
-# In[ ]:
-
-
 ayer=datetime.now()-timedelta(hours=24)
 ayerPayCriteria=ayer.replace( hour=23,minute=59,second=59)
-
-
-# In[ ]:
 
 
 usersCol = mydb_conn["users"]
@@ -65,44 +50,26 @@ usersGot = usersCol_to_dataframes(usersCol.find({},{'_id':1,'_acuerdo':1,'estado
 
 # # ////ACTIVE_SUBSCRIBER ////
 
-# In[ ]:
-
-
 usersGot2=usersGot.loc[~(usersGot['_id'].isnull())]
 usersGot2['_id']=usersGot2['_id'].apply(str)
-
-
-# In[ ]:
 
 
 fullAccess=usersGot2.loc[(usersGot2['estado_token'].isin(['completado','cancelado']))]
 withoutAccess=usersGot2.loc[~(usersGot2['_id'].isin(fullAccess['_id']))]
 
 
-# In[ ]:
-
-
 for index, row in fullAccess.iterrows():
         variablerar2 =usersCol.update_one({"_id":ObjectId(row['_id'])},{ "$set": { "activeSubscriber":True} })
-
-
-# In[ ]:
 
 
 for index, row in withoutAccess.iterrows():
         variablerar2 =usersCol.update_one({"_id":ObjectId(row['_id'])},{ "$set": { "activeSubscriber":False} })
 
 
-# In[ ]:
-
-
 
 
 
 # ### Getting Information from DB
-
-# In[ ]:
-
 
 mycol_transAsig = mydb_conn["transbankfinalizaregistros"]
 def transAsigframes(iterator, chunk_size: int):
@@ -122,9 +89,6 @@ def transAsigframes(iterator, chunk_size: int):
   
 transbank = transAsigframes(mycol_transAsig.find({},{'_id':1,'createdAt':1,'user_id':1}), 10000)
 transbank=transbank.rename(columns={ "createdAt":"START_DATE","_id":"key"})
-
-
-# In[ ]:
 
 
 mycol_paypalAsig= mydb_conn["acuerdospaypals"]
@@ -147,9 +111,6 @@ paypal = paypalAsigframes(mycol_paypalAsig.find({},{'_id':1,'createdAt':1,'user_
 paypal=paypal.rename(columns={ "createdAt":"START_DATE","_id":"key"})
 
 
-# In[ ]:
-
-
 mycol_cupAsig= mydb_conn["cuponesasignados"]
 def cupAsigframes(iterator, chunk_size: int):
   """Turn an iterator into multiple small pandas.DataFrame
@@ -167,9 +128,6 @@ def cupAsigframes(iterator, chunk_size: int):
   return pd.concat(frames) if frames else pd.DataFrame()
 cuponesasignados = cupAsigframes(mycol_cupAsig.find({},{'createdAt':1,'user_id_asignado':1,"cupon_id":1}), 10000)
 cuponesasignados=cuponesasignados.rename(columns={ "createdAt":"START_DATE","cupon_id":"key"})
-
-
-# In[ ]:
 
 
 pagosCol = mydb_conn["pagos"]
@@ -193,15 +151,9 @@ pagos_total = CuponV2_Pagos_to_dataframes(pagosCol.find({},{'user_id':1,'product
 
 # transbanks
 
-# In[ ]:
-
-
 transbank['key']=transbank['key'].apply(str)
 pagos_total['acuerdo_id']=pagos_total['acuerdo_id'].apply(str)
 pagos_transb=pagos_total.loc[pagos_total['acuerdo_id'].isin(transbank['key'])]
-
-
-# In[ ]:
 
 
 ultimoPagoTransbank=pagos_transb.groupby(['user_id']).agg({'fecha_fin': [np.max]}).unstack().reset_index().rename_axis(None, axis=1)
@@ -209,22 +161,13 @@ ultimoPagoTransbank['fecha_fin_pago']=ultimoPagoTransbank[0]
 prelTransbank=ultimoPagoTransbank.drop(['level_0', 'level_1', 0],axis=1)
 
 
-# In[ ]:
-
-
 pagoActivo_transbank=prelTransbank.loc[prelTransbank['fecha_fin_pago']>ayerPayCriteria]
 
 
 # paypals
 
-# In[ ]:
-
-
 paypal['key']=paypal['key'].apply(str)
 pagos_paypals=pagos_total.loc[pagos_total['acuerdo_id'].isin(paypal['key'])]
-
-
-# In[ ]:
 
 
 ultimoPagoPaypal=pagos_paypals.groupby(['user_id']).agg({'fecha_fin': [np.max]}).unstack().reset_index().rename_axis(None, axis=1)
@@ -232,22 +175,13 @@ ultimoPagoPaypal['fecha_fin_pago']=ultimoPagoPaypal[0]
 prelPaypal=ultimoPagoPaypal.drop(['level_0', 'level_1', 0],axis=1)
 
 
-# In[ ]:
-
-
 pagoActivo_paypal=prelPaypal.loc[prelPaypal['fecha_fin_pago']>ayerPayCriteria]
 
 
 # cupones
 
-# In[ ]:
-
-
 cuponesasignados['key']=cuponesasignados['key'].apply(str)
 pagos_cupones=pagos_total.loc[pagos_total['acuerdo_id'].isin(cuponesasignados['key'])]
-
-
-# In[ ]:
 
 
 ultimoPagoCupones=pagos_cupones.groupby(['user_id']).agg({'fecha_fin': [np.max]}).unstack().reset_index().rename_axis(None, axis=1)
@@ -255,27 +189,15 @@ ultimoPagoCupones['fecha_fin_pago']=ultimoPagoCupones[0]
 prelCupones=ultimoPagoCupones.drop(['level_0', 'level_1', 0],axis=1)
 
 
-# In[ ]:
-
-
 pagoActivo_cupones=prelCupones.loc[prelCupones['fecha_fin_pago']>ayerPayCriteria]
-
-
-# In[ ]:
 
 
 pagoActivo_direct = [pagoActivo_transbank, pagoActivo_paypal]
 paidUser=pd.concat(pagoActivo_direct)
 
 
-# In[ ]:
-
-
 paidDirect=usersGot2.loc[(usersGot2['_id'].isin(paidUser['user_id']))]
 NopaidSubscriber=usersGot2.loc[~(usersGot2['_id'].isin(paidUser['user_id']))]
-
-
-# In[ ]:
 
 
 usersCol = mydb_conn["users"]
@@ -286,40 +208,22 @@ for index, row in NopaidSubscriber.iterrows():
     variablerar2 =usersCol.update_many({"_id":ObjectId(row['_id'])},{ "$set":{'paidDirect':False}})
 
 
-# In[ ]:
-
-
 ultimPagoGeneral = [pagoActivo_transbank, pagoActivo_paypal,pagoActivo_cupones]
 LastPaid=pd.concat(ultimPagoGeneral)
 
 
-# In[ ]:
-
-
 pagoActivo=LastPaid.loc[LastPaid['fecha_fin_pago']>ayerPayCriteria]
-
-
-# In[ ]:
 
 
 pagoActivo['user_id']=pagoActivo['user_id'].apply(str)
 usersGot2['_id']=usersGot2['_id'].apply(str)
 
 
-# In[ ]:
-
-
 usersGot2['_id']=usersGot2['_id'].apply(str)
-
-
-# In[ ]:
 
 
 paidSubscriber=usersGot2.loc[(usersGot2['_id'].isin(pagoActivo['user_id']))]
 unpaidSubscriber=usersGot2.loc[~(usersGot2['_id'].isin(pagoActivo['user_id']))]
-
-
-# In[ ]:
 
 
 usersCol = mydb_conn["users"]
@@ -327,9 +231,6 @@ for index, row in paidSubscriber.iterrows():
     variablerar2 =usersCol.update_many({"_id":ObjectId(row['_id'])},{ "$set":{'paidSubscriber':True}})
 for index, row in unpaidSubscriber.iterrows():
     variablerar2 =usersCol.update_many({"_id":ObjectId(row['_id'])},{"$set":{'paidSubscriber':False}})
-
-
-# In[ ]:
 
 
 
